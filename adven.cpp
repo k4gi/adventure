9996b08c7ffa1ba9605fc0ce7f3181e98187473f
kagi <k4gi@pm.me>
2019-05-19 18:08:12 +1000

    add unit struct

--- adven.cpp
+++ adven.cpp
@@ -2,6 +2,7 @@
 #include <cstdlib>
 
 #include "map_loader.h"
+#include "unit.h"
 
 #define FILENAME "testing_map.dat"
 
@@ -17,7 +18,14 @@ int main() {
 	//int pad_y = y*1.5, pad_x = x*1.5;
 
 	int ypos = 0, xpos = 0; //top left corner of the visible map
-	int c_ypos = 2, c_xpos = 2; //position of the player
+
+	unit pc;
+	pc.sym = '@';
+	pc.ypos = 2;
+	pc.xpos = 2;
+	pc.hp = 99;
+	pc.dmg = 1;
+	//int pc.ypos = 2, pc.xpos = 2; //position of the player
 
 	map_loader dan;
 
@@ -42,7 +50,7 @@ int main() {
 
 	WINDOW *map = dan.load_map(FILENAME);
 	
-	mvwaddch(map,c_ypos,c_xpos,'@');
+	mvwaddch(map,pc.ypos,pc.xpos,pc.sym);
 
 	wprintw(log_win,"Welcome to Jason's roguelike, <adven.cpp>!");
 
@@ -59,9 +67,9 @@ int main() {
 		switch(in) {
 		//pickup
 		case 'g':
-			if(mvwinch(dan.getgrid(),c_ypos,c_xpos) == '$') {
+			if(mvwinch(dan.getgrid(),pc.ypos,pc.xpos) == '$') {
 				//inventory management..?
-				mvwaddch(dan.getgrid(),c_ypos,c_xpos,'.');
+				mvwaddch(dan.getgrid(),pc.ypos,pc.xpos,'.');
 				wprintw(log_win,"\nYou grab the dollar sign!");
 			} else {
 				wprintw(log_win,"\nNothing here...");
@@ -76,40 +84,40 @@ int main() {
 			break;
 		//char movement down here
 		case 'w':
-			if(mvwinch(map,c_ypos-1,c_xpos) != '#') {
-				if(ypos>0 && c_ypos-ypos+map_start_y == map_size_y/2) ypos --;
-				mvwaddch(map,c_ypos,c_xpos, mvwinch(dan.getgrid(),c_ypos,c_xpos) );
-				c_ypos --;
-				mvwaddch(map,c_ypos,c_xpos,'@');
+			if(mvwinch(map,pc.ypos-1,pc.xpos) != '#') {
+				if(ypos>0 && pc.ypos-ypos+map_start_y == map_size_y/2) ypos --;
+				mvwaddch(map,pc.ypos,pc.xpos, mvwinch(dan.getgrid(),pc.ypos,pc.xpos) );
+				pc.ypos --;
+				mvwaddch(map,pc.ypos,pc.xpos,pc.sym);
 			}
 			break;
 		case 's':
-			if(mvwinch(map,c_ypos+1,c_xpos) != '#') {
-				if(ypos+map_size_y < dan.gety() && c_ypos-ypos+map_start_y == map_size_y/2) ypos ++;
-				mvwaddch(map,c_ypos,c_xpos, mvwinch(dan.getgrid(),c_ypos,c_xpos) );
-				c_ypos ++;
-				mvwaddch(map,c_ypos,c_xpos,'@');
+			if(mvwinch(map,pc.ypos+1,pc.xpos) != '#') {
+				if(ypos+map_size_y < dan.gety() && pc.ypos-ypos+map_start_y == map_size_y/2) ypos ++;
+				mvwaddch(map,pc.ypos,pc.xpos, mvwinch(dan.getgrid(),pc.ypos,pc.xpos) );
+				pc.ypos ++;
+				mvwaddch(map,pc.ypos,pc.xpos,pc.sym);
 			}
 			break;
 		case 'a':
-			if(mvwinch(map,c_ypos,c_xpos-1) != '#') {
-				if(xpos>0 && c_xpos-xpos+map_start_x == map_size_x/2) xpos --;
-				mvwaddch(map,c_ypos,c_xpos, mvwinch(dan.getgrid(),c_ypos,c_xpos) );
-				c_xpos --;
-				mvwaddch(map,c_ypos,c_xpos,'@');
+			if(mvwinch(map,pc.ypos,pc.xpos-1) != '#') {
+				if(xpos>0 && pc.xpos-xpos+map_start_x == map_size_x/2) xpos --;
+				mvwaddch(map,pc.ypos,pc.xpos, mvwinch(dan.getgrid(),pc.ypos,pc.xpos) );
+				pc.xpos --;
+				mvwaddch(map,pc.ypos,pc.xpos,pc.sym);
 			}
 			break;
 		case 'd':
-			if(mvwinch(map,c_ypos,c_xpos+1) != '#') {
-				if(xpos+map_size_x < dan.getx() && c_xpos-xpos+map_start_x == map_size_x/2) xpos ++;
-				mvwaddch(map,c_ypos,c_xpos, mvwinch(dan.getgrid(),c_ypos,c_xpos) );
-				c_xpos ++;
-				mvwaddch(map,c_ypos,c_xpos,'@');
+			if(mvwinch(map,pc.ypos,pc.xpos+1) != '#') {
+				if(xpos+map_size_x < dan.getx() && pc.xpos-xpos+map_start_x == map_size_x/2) xpos ++;
+				mvwaddch(map,pc.ypos,pc.xpos, mvwinch(dan.getgrid(),pc.ypos,pc.xpos) );
+				pc.xpos ++;
+				mvwaddch(map,pc.ypos,pc.xpos,pc.sym);
 			}
 			break;
 		}
 
-		if(mvwinch(dan.getgrid(),c_ypos,c_xpos) == '$') {
+		if(mvwinch(dan.getgrid(),pc.ypos,pc.xpos) == '$') {
 			wprintw(log_win,"\nYou see a dollar sign on the floor. Press 'g' to pick it up.");
 		}
 
---
+++ unit.h
@@ -0,0 +1,12 @@
+#ifndef UNIT_H
+#define UNIT_H
+
+struct unit {
+	char sym;
+	int ypos;
+	int xpos;
+	int hp;
+	int dmg;
+};
+
+#endif
